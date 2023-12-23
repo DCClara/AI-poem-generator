9cce28b0f3fedf43bb5664db07d43d0fc0a7076c
DCClara <141446800+DCClara@users.noreply.github.com>
2023-12-22 23:21:30 -0500

    added api integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+function displayRecipe(response) {
+  console.log("recipe generated");
+  new Typewriter("#recipe", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 0.5,
+    cursor: "",
+  });
+}
+
 function generateRecipe(event) {
   event.preventDefault();
 
-  new Typewriter("#recipe", {
-    strings: "Béchamel Sauce",
-    autoStart: true,
-    delay: 1,
-    cursor: "",
-  });
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "d494296b1e5a34o4t344f5460f276a60";
+  let prompt = `User instructions : Generate a recipe based on the following ingredients ${instructionsInput.value}`;
+  let context =
+    "You are a professional, world-renowned chef and you love to use ingredients to create recipes that are easy to follow. Your mission is to generate a delicious recipe in basic HTML. Make sure to follow the user instructions. Sign the recipe with 'SheCodes AI' inside a <strong> element at the end";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("generating recipe");
+
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#recipe-generator-form");
