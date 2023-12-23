aaf36758067dc84c905a9a849f36a2bf746260cb
DCClara <141446800+DCClara@users.noreply.github.com>
2023-12-23 01:47:22 -0500

    improved ai prompt and context

--- src/index.js
+++ src/index.js
@@ -13,9 +13,9 @@ function generateRecipe(event) {
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "d494296b1e5a34o4t344f5460f276a60";
-  let prompt = `User instructions : Generate a recipe based on the following ingredients ${instructionsInput.value}`;
+  let prompt = `User instructions : Generate a short recipe based on the following ingredients ${instructionsInput.value}`;
   let context =
-    "You are a professional, world-renowned chef and you love to use ingredients to create recipes that are easy to follow. Your mission is to generate a delicious recipe in basic HTML. Make sure to follow the user instructions. Put the title in a <h2> element. Sign the recipe with 'SheCodes AI 🤖' inside a <strong> element at the end";
+    "You are a professional, world-renowned chef and you love to use ingredients to create recipes that are easy to follow. Your mission is to generate a simple, delicious recipe in basic HTML. Make sure to follow the user instructions. Put the title in a <h2> element. Sign the recipe with 'SheCodes AI 🤖' inside a <strong> element at the end";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let recipeElement = document.querySelector("#recipe");
